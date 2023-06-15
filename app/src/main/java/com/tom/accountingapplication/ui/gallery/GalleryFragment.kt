c58fb1a49200cdbde0cadc38b242a2f2908138cb
joeylee <joey60504@gmail.com>
2023-06-15 10:52:14 +0800

    gradle調整

--- app/src/main/java/com/tom/accountingapplication/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/tom/accountingapplication/ui/gallery/GalleryFragment.kt
@@ -1,6 +1,31 @@
 package com.tom.accountingapplication.ui.gallery
 
 
-class GalleryFragment{
+import android.os.Build
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
+import com.tom.accountingapplication.databinding.FragmentHomeBinding
 
+
+class AccountingFragment : Fragment() {
+//    private val viewModel: AccountingViewModel by viewModels()
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
 }
